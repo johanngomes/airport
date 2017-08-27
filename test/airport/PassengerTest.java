15b01d109e4b28b1fbfeb7bb7957dbb281aeac54
johanngomes <johanngomes@gmail.com>
2017-08-27 15:41:24 -0300

    Book a flight

---
+++ test/airport/PassengerTest.java
@@ -0,0 +1,33 @@
+package airport;
+
+import airport.exceptions.AirportDoesNotExistException;
+import org.junit.Test;
+
+import java.time.LocalTime;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PassengerTest {
+
+    @Test
+    public void shouldBookAFlight() throws AirportDoesNotExistException {
+        Passenger passenger = new Passenger("09660898", "Johann");
+
+        String loc = "KSTVWO";
+        FlightCompanies company = FlightCompanies.GOL;
+
+        AirportFactory airportFactory = new AirportFactory();
+
+        Route route = new Route(airportFactory.getAirport(AirportCode.GIG), airportFactory.getAirport(AirportCode.GRU));
+        TravelTime travelTime = new TravelTime(LocalTime.of(11, 00), LocalTime.of(16, 30));
+        Flight flight = new Flight(loc, company, route, travelTime);
+
+        Booking expectedBooking = new Booking(passenger, flight);
+
+        Booking booking = passenger.book(flight);
+
+        assertThat(booking, is(expectedBooking));
+    }
+
+}
